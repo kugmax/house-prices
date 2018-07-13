fd5a85598ec69966e6b2b6b0989f6af9960cedde
maxim.kugatov <maximkugatov@gmail.com>
2018-07-13 14:58:44 +0300

    progress; first accurency 0.85

--- Runner.py
+++ Runner.py
@@ -7,8 +7,14 @@ from sklearn.decomposition import LatentDirichletAllocation
 from sklearn.preprocessing import LabelEncoder
 from sklearn.manifold import MDS
 from mpl_toolkits.mplot3d import Axes3D
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import minmax_scale
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
 
-np.random.seed(7)
+seed = 7
+np.random.seed(seed)
 
 
 def is_residential(ms_zoning):
@@ -66,10 +72,6 @@ def show_kernel_pca(data, kernel):
     pca.fit(data)
     new_data = pca.transform(data)
 
-    # print(pca.explained_variance_ratio_)
-    # print(pca.singular_values_)
-    # print(pca.noise_variance_)
-
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(new_data[:, 0], new_data[:, 1], new_data[:, 2], marker='.')
@@ -77,7 +79,7 @@ def show_kernel_pca(data, kernel):
 
 
 def show_pca(data):
-    pca = PCA(n_components=2)
+    pca = PCA(n_components=20)
     pca.fit(data)
     new_data = pca.transform(data)
 
@@ -89,11 +91,13 @@ def show_pca(data):
 
     # plt.scatter(n_comp, pca.explained_variance_ratio_, marker='.')
 
-    plt.scatter(new_data[:, 0], new_data[:, 1], marker='.')
+    # plt.scatter(new_data[:, 0], new_data[:, 1], marker='.')
     # fig = plt.figure()
     # ax = fig.add_subplot(111, projection='3d')
     # ax.scatter(new_data[:, 0], new_data[:, 1], new_data[:, 2], marker='.')
-    plt.show()
+    # plt.show()
+
+    return new_data
 
 
 def show_mds(data):
@@ -116,16 +120,74 @@ def show_lda(data):
     plt.show()
 
 
-def main():
+def prepare_data():
     df = pd.read_csv(filepath_or_buffer="resources/train.csv").fillna(0)
+    price = df['SalePrice'].as_matrix()
+
+    df = df.drop(['SalePrice', 'Id'], 1)
+
     encode_labels(df)
     data = df.as_matrix()
 
-    show_pca(data)
+    feature = [np.array(v) for v in data]
+    label = list(map(lambda v: np.array([v]), price))
+
+    return train_test_split(feature, label, test_size=0.3, random_state=seed)
+
+
+def main():
+    train_feature, test_feature, price, test_price = prepare_data()
+
+    lr = LinearRegression()
+    lr.fit(train_feature, price)
+    print('coef_', lr.coef_)
+    print('intercept_', lr.intercept_)
+
+    pred = lr.predict(test_feature)
+
+    accuracy = r2_score(test_price, pred)
+    print('accuracy', accuracy)
+
+    # plt.scatter(train_feature, price, color='b', marker='.')
+    # plt.scatter(test_feature, test_price, color='r', marker='.')
+    # plt.plot(test_feature, pred)
+    # plt.show()
+
+
+def main__():
+    df = pd.read_csv(filepath_or_buffer="resources/train.csv").fillna(0)
+    price = df['SalePrice'].as_matrix()
+    price = minmax_scale(price)
+    df = df.drop(['SalePrice', 'Id'], 1)
+
+    encode_labels(df)
+    data = df.as_matrix()
+
+    pca_data = show_pca(data)
+
+    data = minmax_scale(pca_data)
+
     # for kernel in ["linear", "poly", "rbf", "sigmoid", "cosine", "precomputed"]:
     #     show_kernel_pca(data, kernel)
     # show_lda(data)
     # show_mds(data)
+
+    lr = LinearRegression()
+    selector = RFE(lr)
+    selector.fit(data, price)
+
+    print(selector.support_)
+    print(selector.ranking_)
+
+    # for i in range(len(selector.support_)):
+    #     if selector.support_[i]:
+    #         print(df.iloc[:, [i]].head(1))
+
+    selected_data = selector.transform(data)
+    selected_data = selected_data[0:, 0]
+
+    plt.scatter(selected_data, price, marker='.')
+    plt.show()
 
 
 if __name__ == "__main__":
